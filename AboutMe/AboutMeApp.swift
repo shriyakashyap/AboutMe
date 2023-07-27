5af2b5826692ab765def76484240511ae12941d4
Shriya <shriyakashyap15@gmail.com>
2023-07-27 14:38:56 +0100

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by scholar on 27/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AboutMe/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  AboutMe
+//
+//  Created by scholar on 27/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        ZStack {
+            Color(.systemCyan)
+                .ignoresSafeArea()
+            
+            VStack (alignment: .center, spacing: 20.0) {
+                
+                Text("Hi! I'm Shriya.")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding()
+                
+                Image("doraemon")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .padding()
+            }
+            
+            Button("About me") {
+            }
+            .font(.title)
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
+            
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+}
